e742b13b2baf8165a0015563c29448022a3213f0
HannaKaniuk <136392947+HannaKaniuk@users.noreply.github.com>
2024-07-11 14:12:22 +0300

    initial commit

--- calendars/src/app/app.component.ts
+++ calendars/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calendars';
+  title = 'homes';
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
